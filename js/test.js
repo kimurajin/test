8360f261284178310e052e61e0d1c31871c2b050
jin <kimura.jin@sunborn.co.jp>
2013-07-19 14:14:27 +0900

    フォントサイズを適正値に

--- js/test.js
+++ js/test.js
@@ -4,8 +4,8 @@ $(function() {
   $('h3').css('color','green');
   $('h4').css('color','orange');
 
-  $('h1').css('font-size','20px');
-  $('h2').css('font-size','20px');
+  $('h1').css('font-size','40px');
+  $('h2').css('font-size','30px');
   $('h3').css('font-size','20px');
-  $('h4').css('font-size','20px');
+  $('h4').css('font-size','10px');
 })

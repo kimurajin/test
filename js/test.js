27547fb0cb2a7ad99133f0df620297bb3bff5587
jin <kimura.jin@sunborn.co.jp>
2013-07-19 14:36:23 +0900

    勝手に開発1

--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ $(function() {
   $('h3').css('color','green');
   $('h4').css('color','orange');
 
-  $('h1').css('font-size','40px');
+  $('h1').css('font-size','45px');
   $('h2').css('font-size','30px');
   $('h3').css('font-size','20px');
   $('h4').css('font-size','16px');
